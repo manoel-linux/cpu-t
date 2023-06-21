3145f59733ec35c37ffe1ae58bfdfbae611cc3b0
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-06-21 16:17:21 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-clear
-echo "#############################################################"
-echo "Checking for updates in Void Linux..." 
-echo "#############################################################"
-if ping -q -c 1 -W 1 voidlinux.org >/dev/null; then
-echo "Internet connection established. It is possible to check for updates."
-echo "#############################################################"
 
-read -p "Do you want to update your system? (y/n): " choice
-echo "#############################################################"
-if [[ $choice == "y" || $choice == "Y" ]]; then
-sudo xbps-install -Syu -y
-else
-echo "Skipping system update."
-echo "#############################################################"
-fi
-else
-echo "#############################################################"
-echo "No internet connection. Unable to check for updates Skipping."
-fi
 clear
 echo "#############################################################"
 echo "cpu-t-uninstaller: june 2023"
